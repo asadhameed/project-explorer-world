c05f414d8a37182f0517bdc83138edbfb2672e1a
Asad Hameed <asadhameed2002@yahoo.com>
2021-06-20 05:20:25 +0200

    Handling Errors for Unsupported Routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
+const HttpError = require("./src/models/http-error");
 const placesRouter = require("./src/routes/places-routes");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/places", placesRouter);
+
+/******************************************
+ * Register A new Middleware function
+ * This only runs if we didn't send the response to one of our routes before
+ */
+app.use((req, res, next) => {
+  throw new HttpError("couldn't found Path", 404);
+});
 
 app.use((error, req, res, next) => {
   if (res.headersSend) {
