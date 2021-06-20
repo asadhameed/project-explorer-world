3d26b0376cf8a82817d91c22e5d2129673952438
Asad Hameed <asadhameed2002@yahoo.com>
2021-06-20 03:23:21 +0200

    Getting a Place By User ID

--- backend/src/routes/places-routes.js
+++ backend/src/routes/places-routes.js
@@ -4,11 +4,17 @@ const dummyPlaces = require("../../fakePlace");
 
 const router = express.Router();
 
-router.get("/:placeId", (req, res) => {
-  const { placeId } = req.params;
-  const place = dummyPlaces.find((p) => p.id === placeId);
+router.get("/:pid", (req, res) => {
+  const { pid } = req.params;
+  const place = dummyPlaces.find((p) => p.id === pid);
   if (place) return res.json({ place });
   return res.json({ Message: "No place" });
+});
+
+router.get("/user/:uid", (req, res) => {
+  const { uid } = req.params;
+  const userPlace = dummyPlaces.find((p) => p.creator === uid);
+  res.json({ userPlace });
 });
 
 module.exports = router;
