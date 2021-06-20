cea0970d2e945b17d4159afff63aba9f59391407
Asad Hameed <asadhameed2002@yahoo.com>
2021-06-20 04:05:58 +0200

    Add own error Model

--- backend/src/routes/places-routes.js
+++ backend/src/routes/places-routes.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const dummyPlaces = require("../../fakePlace");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
 router.get("/:pid", (req, res, next) => {
   const { pid } = req.params;
   const place = dummyPlaces.find((p) => p.id === pid);
   if (!place) {
-    return next({
-      code: 404,
-      message: "couldn't find a place for the provide place id",
-    });
+    // return next({
+    //   code: 404,
+    //   message: "couldn't find a place for the provide place id",
+    // });
+    return next(
+      new HttpError("couldn't find a place for the provide place id", 404)
+    );
   }
   return res.json({ place });
 });
@@ -23,9 +28,10 @@ router.get("/user/:uid", (req, res, next) => {
     // return res
     //   .status(404)
     //   .json({ message: "couldn't find a place for the provide user id" });
-    const error = new Error("couldn't find a place for the provide user id");
-    error.code = 404;
-    throw error;
+    // const error = new Error("couldn't find a place for the provide user id");
+    // error.code = 404;
+    // throw error;
+    throw new HttpError("couldn't find a place for the provide user id", 404);
   }
   res.json({ userPlace });
 });
