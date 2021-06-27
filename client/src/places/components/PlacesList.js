76abb78a1aeb74a61ff8c23543dbe4e2feaab750
Asad Hameed <asadhameed2002@yahoo.com>
2021-06-27 07:17:00 +0200

    Share place if user login then go to new place router otherwise redirect to auth

--- client/src/places/components/PlacesList.js
+++ client/src/places/components/PlacesList.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./PlacesList.css";
 import PlaceItem from "./PlaceItem";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/formElements/Button";
+import { AuthContext } from "../../shared/contexts/AuthContext";
 
 const PlacesList = (props) => {
+  const authContext = useContext(AuthContext);
   if (props.places.length === 0) {
     return (
       <div className="center ">
         <Card>
           <h2>No places found. May be create one?</h2>
-          <Button to="/places/new">Share Places</Button>
+          <Button to={authContext.isLoggedIn ? "/places/new" : "/auth"}>
+            Share Places
+          </Button>
         </Card>
       </div>
     );
