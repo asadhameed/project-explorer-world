8ec9ab6f79cb2005ee2735197f6659a255cfab62
Asad Hameed <asadhameed2002@yahoo.com>
2021-06-29 08:18:45 +0200

    Creating a Custom Authentication Hook

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Users from "../users/pages/Users";
 import MainNavigation from "../shared/components/navigation/MainNavigation";
@@ -7,64 +7,10 @@ import Place from "../places/pages/Place";
 import PlaceUpdate from "../places/pages/PlaceUpdate";
 import Auth from "../users/pages/Auth";
 import { AuthContext } from "../shared/contexts/AuthContext";
-let timeOutId;
+import { useAuthHook } from "../shared/hooks/auth-hook";
+
 const App = () => {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userId, setUserId] = useState(null);
-  const [token, setToken] = useState(null);
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-
-  /****************************************************************************
-   * Login and logout working correctly But  it is better useCallback hook
-   * Login and logout function wraps with use callback so that is not
-   * re-create unnecessarily to avoid infinite loops
-   *****************************************************************************/
-  // const login = () => setIsLoggedIn(true);
-  // const logout = () => setIsLoggedIn(false);
-
-  const login = useCallback((uid, token, expiration) => {
-    setUserId(uid);
-    setToken(token);
-    const tokenExpirationDate =
-      expiration || new Date(new Date().getTime() + 1000 * 60 * 60); // server send token which is expire in one hour
-    setTokenExpirationDate(tokenExpirationDate);
-    console.log(tokenExpirationDate);
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({
-        userId: uid,
-        token,
-        expiration: tokenExpirationDate.toISOString(),
-      })
-    );
-  }, []);
-  const logout = useCallback(() => {
-    console.log(tokenExpirationDate);
-    setUserId(null);
-    setToken(null);
-    localStorage.removeItem("userData");
-  }, []);
-
-  useEffect(() => {
-    if (token && tokenExpirationDate) {
-      const remainingTime =
-        tokenExpirationDate.getTime() - new Date().getTime();
-      timeOutId = setTimeout(logout, remainingTime);
-    } else clearTimeout(timeOutId);
-  }, [token, tokenExpirationDate, logout]);
-
-  useEffect(() => {
-    try {
-      const userData = JSON.parse(localStorage.getItem("userData"));
-      const expirationDate = new Date(userData.expiration);
-      if (userData.userId && userData.token && expirationDate > new Date()) {
-        login(userData.userId, userData.token, expirationDate);
-      }
-    } catch (error) {
-      logout();
-    }
-  }, [login, logout]);
-
+  const { token, login, logout, userId } = useAuthHook();
   let route;
   if (token) {
     route = (
---
+++ client/src/shared/hooks/auth-hook.js
@@ -0,0 +1,60 @@
+const { useState, useEffect, useCallback } = require("react");
+
+let timeOutId;
+export const useAuthHook = () => {
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(null);
+  const [token, setToken] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
+
+  /****************************************************************************
+   * Login and logout working correctly But  it is better useCallback hook
+   * Login and logout function wraps with use callback so that is not
+   * re-create unnecessarily to avoid infinite loops
+   *****************************************************************************/
+  // const login = () => setIsLoggedIn(true);
+  // const logout = () => setIsLoggedIn(false);
+
+  const login = useCallback((uid, token, expiration) => {
+    setUserId(uid);
+    setToken(token);
+    const tokenExpirationDate =
+      expiration || new Date(new Date().getTime() + 1000 * 60); // server send token which is expire in one hour
+    setTokenExpirationDate(tokenExpirationDate);
+    console.log(tokenExpirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
+  }, []);
+  const logout = useCallback(() => {
+    setUserId(null);
+    setToken(null);
+    localStorage.removeItem("userData");
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      timeOutId = setTimeout(logout, remainingTime);
+    } else clearTimeout(timeOutId);
+  }, [token, tokenExpirationDate, logout]);
+
+  useEffect(() => {
+    try {
+      const userData = JSON.parse(localStorage.getItem("userData"));
+      const expirationDate = new Date(userData.expiration);
+      if (userData.userId && userData.token && expirationDate > new Date()) {
+        login(userData.userId, userData.token, expirationDate);
+      }
+    } catch (error) {
+      logout();
+    }
+  }, [login, logout]);
+  return { login, logout, userId, token };
+};
